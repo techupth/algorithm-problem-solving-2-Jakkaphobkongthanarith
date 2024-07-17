91282b9976f44dd99d0d17da41e91084dea19da1
Jakkaphob <jkpbeam@gmail.com>
2024-07-17 13:51:38 +0700

    feat: submit ans algo as main

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,24 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i in students) {
+    if (students[i] === searchStudent) {
+      console.log("meow");
+      return i;
+    }
+  }
+  return -1;
 }
 
+students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+console.log("ans is:", findStudentIndex(students, "Diana"));
 // ตอบคำถามตรงนี้จ้า
+//      O(N) เนื่องจากลูปเช็คทุกๆตัวไปเรื่อยๆ ถ้าเจอก็จบตรงนั้น ไม่เจอก็รีเทิร์น-1
